9916b8efaa4e9ad0a7b6039818b881b85eb60534
ahmad18697 <ahmadraza993432@gmail.com>
2025-06-15 15:32:42 +0530

    production url added

--- ahmad-raza-portfolio/src/components/Contact.jsx
+++ ahmad-raza-portfolio/src/components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
     setStatus('submitting');
     
     try {
-      const response = await fetch('https://portfoilio-76j7.vercel.app/api/contact', {
+      const response = await fetch('https://ahmadraza-ashy.vercel.app/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
